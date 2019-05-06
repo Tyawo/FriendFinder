b104a2d29bba3c00b15267656b2918746de03a82
Tyawo <48452575+Tyawo@users.noreply.github.com>
2019-05-05 20:40:26 -0400

    update

--- server.js
+++ server.js
@@ -89,39 +89,7 @@ app.get("/api/friends", function(req, res) {
   return res.json(friends);
 });
 
-// Displays a single friend or returns false
-app.get("/api/friends/:friend", function(req, res) {
-  var chosen = req.params.friend;
 
-  console.log(chosen);
-
-  for (var i = 0; i < friends.length; i++) {
-    if (chosen === friends[i].name) {
-      return res.json(friends[i]);
-    }
-  }
-
-  return res.json(false);
-});
-
-// Create New Characters - takes in JSON input
-app.post("/api/friends", function(req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
-  var newfriend = req.body;
-
-  // Using a RegEx Pattern to remove spaces from newCharacter
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  newfriend.name = newfriend.name.replace(/\s+/g, "").toLowerCase();
-
-  console.log(newfriend);
-
-  friends.push(newfriend);
-
-  res.json(newfriend);
-});
-
-// Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
