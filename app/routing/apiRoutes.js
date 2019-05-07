757d08ce55af4e73d9019e71cbc92911052f58cc
Tyawo <48452575+Tyawo@users.noreply.github.com>
2019-05-07 07:22:26 -0400

    "update apiRoutes.js"

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,9 +5,9 @@ module.exports = function(app) {
 app.get("/api/friends", function(req, res) {
     return res.json(friends);
   });
-  app.post("/api/friends", function(req, res) {
-     if friends 
-  })
+//   app.post("/api/friends", function(req, res) {
+//      if friends 
+//   })
 
 }
 
